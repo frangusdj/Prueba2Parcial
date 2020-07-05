a11a145a6a8c008541fe4081a8bcdd12d6c97ab0
frangusdj <frangusdj@hotmail.com>
2020-07-05 12:39:57 -0500

    Clase empleado

---
+++ Empresa/Empleado.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Empresa{
+    class Empleado{
+        private string nomb, ape, cod;
+        private double sueldo;
+
+        Producto[] productos;
+
+        //Métodos setter
+        public void setNomb(string n) {
+            this.nomb = n;
+        }
+        public void setApe(string a) {
+            this.ape = a;
+        }
+        public void setCod(string c) {
+            this.cod = c;
+        }
+        public void setSueldo(double s) {
+            this.sueldo = s;
+        }
+        //Métodos getter
+        public string getNomb() {
+            return this.nomb;
+        }
+        public string getApe() {
+            return this.ape;
+        }
+        public string getCod() {
+            return this.cod;
+        }
+        public double getSueldo() {
+            return this.sueldo;
+        }
+    }
+}
